1ae36be07610ac6e78836525cfeacf2f4a81cf53
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-11-14 08:56:25 -0300

    feat: create new entities for Answers and question comments

---
+++ src/domain/forum/enterprise/entities/comment.ts
@@ -0,0 +1,38 @@
+import { Entity } from '@/core/entities/entity'
+import type { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+export interface CommentProps {
+  content: string
+  authorId: UniqueEntityID
+  createdAt: Date
+  updatedAt?: Date
+}
+
+export abstract class Comment<T extends CommentProps> extends Entity<T> {
+  get content() {
+    return this.props.content
+  }
+
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+}
