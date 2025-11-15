72473562e77375aed0b2dd5b4d71bedf2b157528
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-11-15 10:54:00 -0300

    feat: add comment on answer use case

---
+++ src/domain/forum/application/use-cases/comment-on-answer.ts
@@ -0,0 +1,45 @@
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { AnswerComment } from "../../enterprise/entities/answer-comment";
+import type { AnswersRepository } from "../repositories/answers-repository";
+import type { AnswerCommentsRepository } from "../repositories/answer-comments-repository";
+
+interface CommentOnAnswerUsecaseRequest {
+    content: string;
+    authorId: string;
+    answerId: string;
+}
+interface CommentOnAnswerUsecaseResponse {
+    answerComment: AnswerComment
+ }
+
+
+export class CommentOnAnswerUsecase {
+
+    constructor(
+        private answersRepository: AnswersRepository,
+        private answerCommentsRepository: AnswerCommentsRepository
+    ) { }
+
+    async exec({ authorId, content, answerId }: CommentOnAnswerUsecaseRequest): Promise<CommentOnAnswerUsecaseResponse> { 
+
+        const answer = this.answersRepository.findById(answerId)
+
+        if(!answer) {
+            throw new Error("Answer not found.")
+        }
+
+        const answerComment = AnswerComment.create({
+            authorId: new UniqueEntityID(authorId),
+            answerId: new UniqueEntityID(answerId),
+            content
+        })
+
+        await this.answerCommentsRepository.create(answerComment)
+
+        return {
+            answerComment
+        }
+
+    }
+
+}
