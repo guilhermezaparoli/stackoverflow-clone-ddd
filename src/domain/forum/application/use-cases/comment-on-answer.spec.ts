cc083e15d45102cfab346d37c9a624edce2d9d28
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-11-15 10:54:11 -0300

    test: add unit tests for comment on answer feature

---
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,34 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository';
+
+import { makeQuestion } from 'test/factories/make-question';
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository';
+import { CommentOnAnswerUsecase } from './comment-on-answer';
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository';
+import { makeAnswer } from 'test/factories/make-answer';
+
+
+let answersRepository: InMemoryAnswersRepository;
+let answerCommentsRepository : InMemoryAnswerCommentsRepository;
+let sut: CommentOnAnswerUsecase;
+
+describe("Comment on Answer", () => {
+
+    beforeEach(() => {
+        answersRepository = new InMemoryAnswersRepository()
+        answerCommentsRepository = new InMemoryAnswerCommentsRepository()
+        sut = new CommentOnAnswerUsecase(answersRepository, answerCommentsRepository)
+    })
+    it('should be able to comment on answer', async () => {
+        const answer = makeAnswer()
+
+        answersRepository.create(answer)
+
+       await sut.exec({
+            authorId: answer.authorId.toString(),
+            answerId: answer.id.toString(),
+            content: "Comentário"
+        })
+
+        expect(answerCommentsRepository.items[0]?.content).toEqual("Comentário")
+    })
+})
