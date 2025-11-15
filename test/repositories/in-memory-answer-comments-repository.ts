5d364fcdefcac6e64c13b388c5e886fe49d36a1e
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-11-15 10:53:49 -0300

    test: add in-memory repository for answer comments

---
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -0,0 +1,11 @@
+import type { AnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
+import type { AnswerComment } from "@/domain/forum/enterprise/entities/answer-comment";
+
+export class InMemoryAnswerCommentsRepository implements AnswerCommentsRepository {
+    public items: AnswerComment[] = []
+    
+    async create(answer: AnswerComment): Promise<void> {
+        this.items.push(answer)
+    }
+    
+}
