ce879fbbad9a31395b1543702c174c5a9d3475da
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-11-15 10:41:58 -0300

    feat: add question comments repository interface

---
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -0,0 +1,6 @@
+import type { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+
+
+export interface QuestionCommentsRepository {
+  create(questionComment: QuestionComment): Promise<void>
+}
