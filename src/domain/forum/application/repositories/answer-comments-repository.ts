32065b1a1eb526f07b90754219c26510a1318c40
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-11-15 10:53:31 -0300

    feat: add repository interface for answer comments

---
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -0,0 +1,6 @@
+import type { AnswerComment } from '../../enterprise/entities/answer-comment'
+
+
+export interface AnswerCommentsRepository {
+  create(AnswerComment: AnswerComment): Promise<void>
+}
