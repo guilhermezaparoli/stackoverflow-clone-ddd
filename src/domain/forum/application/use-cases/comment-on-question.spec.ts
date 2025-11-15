508d16a43e51cb5cc7402814d67159fba1408835
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-11-15 10:42:34 -0300

    test: add unit tests for question commenting feature

---
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,33 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository';
+
+import { makeQuestion } from 'test/factories/make-question';
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository';
+import { CommentOnQuestionUsecase } from './comment-on-question';
+
+
+let questionsRepository: InMemoryQuestionsRepository;
+let questionCommentsRepository : InMemoryQuestionCommentsRepository;
+let sut: CommentOnQuestionUsecase;
+
+describe("Comment on Question", () => {
+
+    beforeEach(() => {
+        questionsRepository = new InMemoryQuestionsRepository()
+        questionCommentsRepository = new InMemoryQuestionCommentsRepository()
+        sut = new CommentOnQuestionUsecase(questionsRepository, questionCommentsRepository)
+    })
+    it('should be able to comment on question', async () => {
+        const question = makeQuestion()
+
+        
+        questionsRepository.create(question)
+
+       const { questionComment } = await sut.exec({
+            authorId: question.authorId.toString(),
+            questionId: question.id.toString(),
+            content: "Comentário"
+        })
+
+        expect(questionCommentsRepository.items[0]?.content).toEqual("Comentário")
+    })
+})
