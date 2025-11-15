70b432861e1877c69c88a29cfbe1f7600e216d88
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-11-15 11:33:09 -0300

    feat: add question comment factory and repository methods

---
+++ test/factories/make-question-comment.ts
@@ -0,0 +1,18 @@
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { type CommentProps } from "@/domain/forum/enterprise/entities/comment";
+import { QuestionComment } from "@/domain/forum/enterprise/entities/question-comment";
+import { faker } from "@faker-js/faker";
+
+export function makeQuestionComment(
+    override: Partial<CommentProps> = {},
+    id?: UniqueEntityID
+) {
+    const questionComment = QuestionComment.create({
+        authorId: new UniqueEntityID(),
+        content: faker.lorem.text(),
+        questionId: new UniqueEntityID(),
+        ...override
+    }, id)
+
+    return questionComment
+}
--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -3,9 +3,25 @@ import type { QuestionComment } from "@/domain/forum/enterprise/entities/questio
 
 export class InMemoryQuestionCommentsRepository implements QuestionCommentsRepository {
     public items: QuestionComment[] = []
-    
+
     async create(question: QuestionComment): Promise<void> {
         this.items.push(question)
     }
-    
+
+    async delete(questionComment: QuestionComment): Promise<void> {
+        const itemIndex = this.items.findIndex((item) => item.id === questionComment.id)
+
+        this.items.splice(itemIndex, 1)
+    }
+
+    async findById(id: string): Promise<QuestionComment | null> {
+        const comment = this.items.find((item) => item.id.toString() === id)
+
+        if (!comment) {
+            return null
+        }
+
+        return comment
+    }
+
 }
