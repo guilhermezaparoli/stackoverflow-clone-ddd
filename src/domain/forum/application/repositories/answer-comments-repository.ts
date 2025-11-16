1bf1e782d763f7e5fc4b40bcaee10fc6cfee3ccc
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-11-16 11:17:16 -0300

    feat: add pagination support for answer comments

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,7 +1,12 @@
+import type { PaginationParams } from '@/core/repositories/pagination-params'
 import type { AnswerComment } from '../../enterprise/entities/answer-comment'
 
 export interface AnswerCommentsRepository {
   create(AnswerComment: AnswerComment): Promise<void>
   findById(id: string): Promise<AnswerComment | null>
   delete(answerComment: AnswerComment): Promise<void>
+  findManyByAnswerId(
+    answerId: string,
+    params: PaginationParams,
+  ): Promise<AnswerComment[]>
 }
