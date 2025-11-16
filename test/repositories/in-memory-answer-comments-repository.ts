b8ce4aebe3af86f64f73344c6b1e870f042733a4
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-11-16 10:30:56 -0300

    feat: add answer comment factory and repository methods

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -7,5 +7,23 @@ export class InMemoryAnswerCommentsRepository implements AnswerCommentsRepositor
     async create(answer: AnswerComment): Promise<void> {
         this.items.push(answer)
     }
+
+
+        async delete(answerComment: AnswerComment): Promise<void> {
+            const itemIndex = this.items.findIndex((item) => item.id === answerComment.id)
+    
+            this.items.splice(itemIndex, 1)
+        }
+    
+        async findById(id: string): Promise<AnswerComment | null> {
+            const comment = this.items.find((item) => item.id.toString() === id)
+    
+            if (!comment) {
+                return null
+            }
+    
+            return comment
+        }
+    
     
 }
