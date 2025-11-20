21e8bfafa51e69f61dacd86e2a250b1ab7480495
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-11-19 21:03:22 -0300

    refactor: update delete comment question use case tests to use Either type for error handling

--- src/domain/forum/application/use-cases/delete-comment-question.spec.ts
+++ src/domain/forum/application/use-cases/delete-comment-question.spec.ts
@@ -2,6 +2,8 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { makeQuestionComment } from 'test/factories/make-question-comment'
 import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
 import { DeleteCommentQuestionUseCase } from './delete-comment-question'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { NotAllowedError } from './errors/not-allowed-error'
 
 let questionsCommentsRepository: InMemoryQuestionCommentsRepository
 let sut: DeleteCommentQuestionUseCase
@@ -18,10 +20,12 @@ describe('Delete comment on question', () => {
 
     questionsCommentsRepository.create(comment)
 
-    await sut.exec({
+   const result =  await sut.exec({
       authorId: 'author-1',
       commentId: comment.id.toString(),
     })
+
+    expect(result.isRight()).toBe(true)
     expect(questionsCommentsRepository.items).toHaveLength(0)
   })
   it('should not be able to delete a comment on a question from another user', async () => {
@@ -31,13 +35,15 @@ describe('Delete comment on question', () => {
 
     questionsCommentsRepository.create(comment)
 
-    await expect(() =>
-      sut.exec({
+    const result = await sut.exec({
         authorId: 'author-2',
         commentId: comment.id.toString(),
-      }),
-    ).rejects.instanceOf(Error)
-  })
+      })
+
+      expect(result.isLeft()).toBe(true)
+      expect(result.value).toBeInstanceOf(NotAllowedError)
+    
+    })
   it("should not be able to delete a comment that doesn't exists", async () => {
     const comment = makeQuestionComment({
       authorId: new UniqueEntityID('author-1'),
@@ -45,11 +51,11 @@ describe('Delete comment on question', () => {
 
     questionsCommentsRepository.create(comment)
 
-    await expect(() =>
-      sut.exec({
+    const result = await sut.exec({
         authorId: 'author-2',
         commentId: '123',
-      }),
-    ).rejects.instanceOf(Error)
+      })
+   expect(result.isLeft()).toBe(true)
+   expect(result.value).toBeInstanceOf(ResourceNotFoundError)
   })
 })
