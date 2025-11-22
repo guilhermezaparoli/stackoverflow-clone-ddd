f7cfc15c92f0247f6ca1b2098ad60fca8bf9e859
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-11-22 17:16:13 -0300

    test: add attachment deletion verification to delete answer use case tests

--- src/domain/forum/application/use-cases/delete-answer.spec.ts
+++ src/domain/forum/application/use-cases/delete-answer.spec.ts
@@ -1,16 +1,20 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
 import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
 import { DeleteAnswerUseCase } from './delete-answer'
-import { makeAnswer } from 'test/factories/make-answer'
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { NotAllowedError } from './errors/not-allowed-error'
+import { makeAnswerAttachment } from 'test/factories/maker-answer-attachment'
 
 let answersRepository: InMemoryAnswersRepository
+let asnwerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
 let sut: DeleteAnswerUseCase
 
 describe('Delete answer', () => {
   beforeEach(() => {
+    asnwerAttachmentsRepository = new InMemoryAnswerAttachmentsRepository()
     answersRepository = new InMemoryAnswersRepository()
-    sut = new DeleteAnswerUseCase(answersRepository)
+    sut = new DeleteAnswerUseCase(answersRepository, asnwerAttachmentsRepository)
   })
   it('should be able to delete a answer', async () => {
     const newAnswer = makeAnswer(
@@ -18,6 +22,21 @@ describe('Delete answer', () => {
         authorId: new UniqueEntityID('author-1'),
       },
       new UniqueEntityID('answer-1'),
+    )
+
+    asnwerAttachmentsRepository.items.push(
+      makeAnswerAttachment(
+        {
+          answerId: newAnswer.id,
+          attachmentId: new UniqueEntityID('1')
+        },
+      ),
+      makeAnswerAttachment(
+        {
+          answerId: newAnswer.id,
+          attachmentId: new UniqueEntityID('2')
+        },
+      )
     )
 
     await answersRepository.create(newAnswer)
@@ -29,6 +48,7 @@ describe('Delete answer', () => {
 
     expect(result.isRight()).toBe(true)
     expect(answersRepository.items).length(0)
+    expect(asnwerAttachmentsRepository.items).toHaveLength(0)
   })
   it('should not be able to delete a answer from another user', async () => {
     const newanswer = makeAnswer(
