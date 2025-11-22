12939c3e39b0868d76588a13af58585dc8ef2fc6
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-11-22 16:26:01 -0300

    test: add attachment management verification to edit answer use case tests

--- src/domain/forum/application/use-cases/edit-answer.spec.ts
+++ src/domain/forum/application/use-cases/edit-answer.spec.ts
@@ -3,26 +3,45 @@ import { makeAnswer } from 'test/factories/make-answer'
 import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
 import { EditAnswerUseCase } from './edit-answer'
 import { NotAllowedError } from './errors/not-allowed-error'
+import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
+import { makeAnswerAttachment } from 'test/factories/maker-answer-attachment'
 
 let answersRepository: InMemoryAnswersRepository
+let answerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
 let sut: EditAnswerUseCase
 
 describe('Edit Answer', () => {
   beforeEach(() => {
+    answerAttachmentsRepository = new InMemoryAnswerAttachmentsRepository()
     answersRepository = new InMemoryAnswersRepository()
-    sut = new EditAnswerUseCase(answersRepository)
+    sut = new EditAnswerUseCase(answersRepository, answerAttachmentsRepository)
   })
   it('should be able to edit a answer', async () => {
-    const createdAnswer = makeAnswer({
-      authorId: new UniqueEntityID('author-1'),
-    })
+    const createdAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
 
     answersRepository.create(createdAnswer)
+
+    answerAttachmentsRepository.items.push(
+      makeAnswerAttachment({
+        answerId: createdAnswer.id,
+        attachmentId: new UniqueEntityID('1'),
+      }),
+      makeAnswerAttachment({
+        answerId: createdAnswer.id,
+        attachmentId: new UniqueEntityID('2'),
+      }),
+    )
 
     const result = await sut.exec({
       authorId: createdAnswer.authorId.toString(),
       content: 'New content',
       answerId: createdAnswer.id.toString(),
+      attachmentsIds: ['1', '3'],
     })
 
     expect(result.isRight()).toBe(true)
@@ -31,6 +50,15 @@ describe('Edit Answer', () => {
         content: 'New content',
       }),
     })
+    expect(answersRepository.items[0]?.attachments.currentItems).toHaveLength(2)
+    expect(answersRepository.items[0]?.attachments.currentItems).toEqual([
+      expect.objectContaining({
+        attachmentId: new UniqueEntityID('1'),
+      }),
+      expect.objectContaining({
+        attachmentId: new UniqueEntityID('3'),
+      }),
+    ])
   })
   it('should not be able to edit a answer from another author', async () => {
     const createdAnswer = makeAnswer({
@@ -44,6 +72,7 @@ describe('Edit Answer', () => {
       authorId: 'author-2',
       content: 'New content',
       answerId: createdAnswer.id.toString(),
+      attachmentsIds: [],
     })
 
     expect(result.isLeft()).toBe(true)
