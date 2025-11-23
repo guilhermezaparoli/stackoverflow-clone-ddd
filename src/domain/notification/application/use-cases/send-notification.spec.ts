3a9ef8022241b2352a62adf3dd6eedadbc2f4bfd
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-11-23 11:41:43 -0300

    feat: add send notification use case test with notification creation validation and repository verification

---
+++ src/domain/notification/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,31 @@
+import { InMemoryNotificationsRepository } from "test/repositories/in-memory-notifications-repository"
+import { SendNotificationUseCase } from "./send-notification"
+import { makeNotification } from "test/factories/make-notification"
+
+
+
+let notificationsRepository: InMemoryNotificationsRepository
+
+let sut: SendNotificationUseCase
+
+describe('Send notification Use Case', () => {
+
+    beforeEach(() => {
+        notificationsRepository = new InMemoryNotificationsRepository()
+        sut = new SendNotificationUseCase(notificationsRepository)
+    })
+
+    it('should be able to create a notification', async () => {
+
+
+        const result =await sut.exec({
+            content: 'notification content',
+            title: 'notification title',
+            recipientId: '123'
+        })
+
+        expect(result.isRight()).toBe(true)
+        expect(notificationsRepository.items[0]).toEqual(result.value?.notification)
+
+    })
+})
