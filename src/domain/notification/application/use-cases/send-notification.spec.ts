5b80abf3a40bc1ff56d3833e091fe47e100645b1
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-11-24 07:46:48 -0300

    feat: format send notification test with consistent indentation, spacing, and quote style

--- src/domain/notification/application/use-cases/send-notification.spec.ts
+++ src/domain/notification/application/use-cases/send-notification.spec.ts
@@ -1,31 +1,25 @@
-import { InMemoryNotificationsRepository } from "test/repositories/in-memory-notifications-repository"
-import { SendNotificationUseCase } from "./send-notification"
-import { makeNotification } from "test/factories/make-notification"
-
-
+import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository'
+import { SendNotificationUseCase } from './send-notification'
+import { makeNotification } from 'test/factories/make-notification'
 
 let notificationsRepository: InMemoryNotificationsRepository
 
 let sut: SendNotificationUseCase
 
 describe('Send notification Use Case', () => {
+  beforeEach(() => {
+    notificationsRepository = new InMemoryNotificationsRepository()
+    sut = new SendNotificationUseCase(notificationsRepository)
+  })
 
-    beforeEach(() => {
-        notificationsRepository = new InMemoryNotificationsRepository()
-        sut = new SendNotificationUseCase(notificationsRepository)
+  it('should be able to create a notification', async () => {
+    const result = await sut.exec({
+      content: 'notification content',
+      title: 'notification title',
+      recipientId: '123',
     })
 
-    it('should be able to create a notification', async () => {
-
-
-        const result =await sut.exec({
-            content: 'notification content',
-            title: 'notification title',
-            recipientId: '123'
-        })
-
-        expect(result.isRight()).toBe(true)
-        expect(notificationsRepository.items[0]).toEqual(result.value?.notification)
-
-    })
+    expect(result.isRight()).toBe(true)
+    expect(notificationsRepository.items[0]).toEqual(result.value?.notification)
+  })
 })
