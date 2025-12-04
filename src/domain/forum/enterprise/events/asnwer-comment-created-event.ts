c083d85f90ef9b27a1995ee90d5d0c3cfb5c8da4
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-12-04 08:19:17 -0300

    feat: add AnswerCommentCreatedEvent domain event with answer comment aggregate, occurrence timestamp, and aggregate ID retrieval

---
+++ src/domain/forum/enterprise/events/asnwer-comment-created-event.ts
@@ -0,0 +1,17 @@
+import type { DomainEvent } from "@/core/events/domain-event";
+import type { AnswerComment } from "../entities/answer-comment";
+import type { UniqueEntityID } from "@/core/entities/unique-entity-id";
+
+export class AnswerCommentCreatedEvent implements DomainEvent {
+    ocurredAt: Date;
+    answerComment: AnswerComment
+
+    constructor(answerComment: AnswerComment){
+        this.answerComment = answerComment
+        this.ocurredAt = new Date()
+    }
+
+    getAggregateId(): UniqueEntityID {
+        return this.answerComment.id
+    }
+}
